361d53730258aa1946ab436791deae4a58c81811
Emon Ahmed <imonshomon@gmail.com>
2024-02-20 09:34:16 +0600

    Done problem

--- some.js.js
+++ some.js.js
@@ -1,11 +1,48 @@
 // inch to foot
 
-const inch = 24;
+// const inch = 24;
 
-function Tooo(inchTofoot) {
+// function Tooo(inchTofoot) {
 
-    const foot = parseInt(inchTofoot / 12)
-    return foot
+//     const foot = parseInt(inchTofoot / 12)
+//     return foot
+
+// }
+// console.log(Tooo(inch))
+
+//if odd numer retrun flase if even number retrun true
+
+
+
+// function cheakEvenOdd(numer) {
+
+//     const remainder = numer % 2
+//     console.log(remainder)
+//     if (remainder == 0) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+
+// console.log(cheakEvenOdd(5))
+
+
+//leap when year is leapyear
+
+
+function Leapyear(year) {
+
+    const Leap = year % 4
+
+    if (Leap === 0) {
+        return console.log("this is  leap year")
+    }
+
+    else {
+        return console.log("this is not  leap year")
+    }
 
 }
-console.log(Tooo(inch))
+Leapyear(2024)
