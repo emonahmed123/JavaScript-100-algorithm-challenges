5af5984793233b272bb6894f27b2c967c0204ed0
Emon Ahmed <imonshomon@gmail.com>
2024-02-20 08:47:31 +0600

    solve one more question

---
+++ problem-four.js
@@ -0,0 +1,12 @@
+function secuence(inputArray) {
+    for (let i = 1; i < inputArray.length; i++) {
+
+        if (inputArray[i] < inputArray[i - 1]) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(secuence([1, 2, 3, 4]))
+// console.log(secuence([1, 5, 3, 4]))
---
+++ some.js.js
@@ -0,0 +1,11 @@
+// inch to foot
+
+const inch = 24;
+
+function Tooo(inchTofoot) {
+
+    const foot = parseInt(inchTofoot / 12)
+    return foot
+
+}
+console.log(Tooo(inch))
