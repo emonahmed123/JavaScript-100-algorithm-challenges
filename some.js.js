d2a2c8d4617f80537e62f98a9691aece770e7408
Emon Ahmed <imonshomon@gmail.com>
2024-02-27 16:06:29 +0600

    done fibonacii sequence

--- some.js.js
+++ some.js.js
@@ -327,3 +327,14 @@
 // }
 
 // console.log(ReversWord('I love you'))
+
+
+// Fibonacci sequence
+
+
+// let fibo = [0, 1]
+
+// for (let i = 2; i < 13; i++) {
+//     fibo[i] = fibo[i - 1] + fibo[i - 2]
+// }
+// console.log(fibo)
