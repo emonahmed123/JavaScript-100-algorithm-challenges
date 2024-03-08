909fefcc39097de2a6d3d9f9cf3cc9c4110c27db
Emon Ahmed <imonshomon@gmail.com>
2024-03-08 17:24:11 +0600

    done

--- some.js.js
+++ some.js.js
@@ -591,4 +591,34 @@
 
 // countProperties({ name: "John", age: 30, city: "New York" })
 
+// Extract Names from Array of Objects
+// You are given an array of objects, where each object represents a person with properties such as name and age.
 
+// Your task is to write a JavaScript function that extracts the names of all the individuals from the array and returns them as an array of strings.
+
+// Write a function named extractNames that takes an array of objects as input and returns an array containing the names of all the individuals from the array.
+
+
+// function extractNames(array) {
+//     let arr = []
+
+
+//     const arrMap = array.map(ar => arr.push(ar.name))
+
+//     return arr
+// }
+
+
+// console.log(extractNames([{
+//     name: "abul",
+//     age: 20,
+// }, {
+//     name: "kabul",
+//     age: 25,
+// }, {
+//     name: "babul",
+//     age: 23,
+// }, {
+//     name: "bulbul",
+//     age: 21,
+// }]))
