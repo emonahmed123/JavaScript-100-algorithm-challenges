a65485ca15af7942374625a24a55d9ee11ee7bbe
Emon Ahmed <imonshomon@gmail.com>
2024-03-08 17:12:39 +0600

    done

--- some.js.js
+++ some.js.js
@@ -581,3 +581,14 @@
 // You are given an object with various properties.Your task is to write a JavaScript function that counts the number of properties in the object and returns the count.
 
 // Write a function named countProperties that takes an object as an argument and returns the number of properties it contains.
+
+
+// function countProperties(object) {
+//     const property = Object.keys(object).length
+//     console.log(property)
+
+// }
+
+// countProperties({ name: "John", age: 30, city: "New York" })
+
+
