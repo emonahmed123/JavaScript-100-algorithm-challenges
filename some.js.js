d2eca52473724ad07cb0a806ce56a18a28833e40
Emon Ahmed <imonshomon@gmail.com>
2024-03-28 16:34:21 +0600

    done

--- some.js.js
+++ some.js.js
@@ -715,19 +715,113 @@
 
 //     Hints: First check if your output is in number format or string format by typeof operator.
 
-function average(arrOfMarks) {
+// function average(arrOfMarks) {
 
-    let total = 0
+//     let total = 0
 
-    for (let i = 0; i < arrOfMarks.length; i++) {
+//     for (let i = 0; i < arrOfMarks.length; i++) {
 
-        total = total + arrOfMarks[i]
+//         total = total + arrOfMarks[i]
 
+//     }
+
+//     let gor = total / arrOfMarks.length
+//     let totalGor = parseFloat(gor.toFixed(2))
+//     return totalGor
+// }
+
+// console.log(average([75.25, 65, 80, 35.45, 99.50]));
+// function stringCombine(str1, str2) {
+//     //write your code here
+//     //don't forget to write return
+
+
+
+//     const str3 = str1 + ' ' + str2;
+
+//     console.log(str3)
+// }
+
+// console.log(stringCombine('I am going to be', 'an awesome web developer'));
+
+// Conditional Statements: Switch
+// Objective
+// In this challenge, we learn about switch statements.
+
+// Task
+// Complete the getLetter(s) function in the editor. It has one parameter: a string, 
+// , consisting of lowercase English alphabetic letters (i.e., a through z). It must return A, B, C, or D depending on the following criteria:
+
+// If the first character in string s is in the set {a , e, i, o, u} then return A.
+// If the first character in string s is in the set {b , c, d, f, g} then return B.
+// If the first character in string s is in the set {h , j , k, l, m} then return C.
+// If the first character in string s is in the set {n , p , q , r , s , t , v , w , x , y , z} then return D.
+// Hint: You can get the letter at some index i in s using the syntax s[i] or s.charAt(i).
+
+// Function Description
+// Complete the getLetter function in the editor .
+// getLetter has the following parameters:
+
+// string s: a string
+// Returns
+
+// string: a single letter determined as described above
+// Input Format
+// A single string denoting s .
+
+
+// Constraints
+
+// 1<=|s|<=100, where |s| is the length of s.
+// String s contains lowercase English alphabetic letters (i.e., a through z) only.
+// Sample Input 0
+
+// adfgt
+// Sample Output 0
+
+// A
+// Explanation 0
+
+// The first character of string s = adfgt is a. Because the given criteria stipulate that we print A any time the first character is in {a,e,i,o,u} , we return A as our answer.
+
+
+
+
+
+function getLetter(s) {
+    let firstChar = s.charAt(0);
+    let letter;
+    switch (firstChar) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = 'A';
+            break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = 'B';
+            break;
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = 'C';
+            break;
+        default:
+            letter = 'D';
     }
-
-    let gor = total / arrOfMarks.length
-    let totalGor = parseFloat(gor.toFixed(2))
-    return totalGor
+    return letter;
 }
 
-console.log(average([75.25, 65, 80, 35.45, 99.50]));
+
+let s = "adfgt";
+
+console.log(getLetter(s));
+
+
