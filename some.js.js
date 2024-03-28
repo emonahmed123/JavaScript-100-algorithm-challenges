d00d60030c81a6ca252671249386627c62c6a3be
Emon Ahmed <imonshomon@gmail.com>
2024-03-28 11:08:06 +0600

    try to slove

--- some.js.js
+++ some.js.js
@@ -679,3 +679,13 @@
 
 //  today nocoding for ramadan
 
+// Learn how to create a temperature converter. Your task is to provide a value in Fahrenheit and then convert it into Celsius . Then round it up to two decimal places if you get fraction. And return the rounded value as output.
+
+
+
+// Alert: The output must be in a number format.
+
+
+
+// The formula below shows how to convert from Fahrenheit to Celsius temperature measurements:
+
