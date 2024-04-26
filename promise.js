b401762f7f0709d6bae1bdb957ada647e9d47364
Emon Ahmed <imonshomon@gmail.com>
2024-04-27 02:15:54 +0800

    done

--- promise.js
+++ promise.js
@@ -1,8 +1,34 @@
-const getData = new Promise((resolve, reject) => {
-    reject('no data')
-})
-console.log(getData)
 
-getData
-    .then(data => console.log(data + 2))
-    .catch(err => console.log('Err', err))
+
+
+
+// const getData = new Promise((resolve, reject) => {
+//     const num = Math.random() * 10;
+//     console.log(num)
+//     if (num < 5) {
+
+
+
+//         resolve('33')
+
+//     }
+//     else (
+//         reject('no data')
+//     )
+
+
+// })
+// console.log(getData)
+
+// getData
+//     .then(data => console.log('loade', data))
+//     .catch(err => console.log('Err', err))
+
+
+async function myfnc() {
+    throw new Error(['tt'])
+}
+
+const res = myfnc()
+
+console.log(res)
