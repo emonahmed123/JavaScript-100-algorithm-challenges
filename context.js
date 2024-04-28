52e9f8d3d571b5c9fdda67291c6a94793a088486
Emon Ahmed <imonshomon@gmail.com>
2024-04-28 21:02:53 +0600

    done

--- context.js
+++ context.js
@@ -1,11 +1,50 @@
-function sayName() {
-    var name = 'emon';
-    console.log('The name is ', name);
-    const helllo = null
-    // that was very good
+// function sayName() {
+//     var name = 'emon';
+//     console.log('The name is ', name);
+//     const helllo = null
+//     // that was very good
+// }
+// sayName()
+
+// const newPromise = new Promise((res, rej) => {
+
+// })
+
+
+// console.log('Inside global execution context');
+
+// var a = 5;
+
+// function testMe() {
+//     var b = 10;
+//     var user = {
+//         name: "emon",
+//         country: "bangladesh"
+//     }
+
+//     function testAgain() {
+//         console.log("Inside testAgin exction eontext");
+//         console.log("Exiting testAgain exction context")
+//     }
+//     testAgain();
+//     console.log("Exiting testMe exection context");
+
+// }
+// testMe()
+
+// console.log("Exiting global excution context")
+
+
+function first() {
+    return "frist"
 }
-sayName()
 
-const newPromise = new Promise((res, rej) => {
+function second() {
+    return "second"
+}
 
-})
+
+
+const masseage = first() && second();
+
+console.log(masseage)
