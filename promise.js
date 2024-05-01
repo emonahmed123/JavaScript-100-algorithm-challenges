8284486c3c51686f90be2b7334b1e1f17aa32235
Emon Ahmed <imonshomon@gmail.com>
2024-05-01 12:53:30 +0600

    done

--- promise.js
+++ promise.js
@@ -2,33 +2,77 @@
 
 
 
-// const getData = new Promise((resolve, reject) => {
-//     const num = Math.random() * 10;
-//     console.log(num)
-//     if (num < 5) {
+const getData = new Promise((resolve, reject) => {
+    const reS = document.getElementById("Res");
+    const reJ = document.getElementById("Rej");
+
+    reS.addEventListener('click', () => {
+
+        resolve('hello')
+    })
+    reJ.addEventListener('click', () => {
+
+        reject('jee')
+    })
+
+
+    // const num = Math.random() * 10;
+
+    // console.log(num)
+    // if (num < 5) {
 
 
 
-//         resolve('33')
+    //     resolve('33')
 
-//     }
-//     else (
-//         reject('no data')
-//     )
+    // }
+    // else (
+    //     reject('no data')
+    // )
 
 
-// })
+
+
+
+})
+console.log(getData)
+
+getData
+    .then(data => console.log('loade', data))
+    .catch(err => console.log('Err', err))
+
 // console.log(getData)
+// async function myfnc() {
+//     setTimeout(() => {
 
-// getData
-//     .then(data => console.log('loade', data))
-//     .catch(err => console.log('Err', err))
+//     }, 2000)
+
+//     return 'hello'
+// }
+
+// const res = myfnc().then()
+
+// console.log(res)
+// console.log(myfnc())
 
 
-async function myfnc() {
-    throw new Error(['tt'])
-}
+// const myPromise = new Promise((resolve, reject) => {
+//     // Simulate an asynchronous operation
+//     setTimeout(() => {
+//         const success = false;
+//         if (success) {
+//             resolve("Operation completed successfully!");
+//         } else {
+//             reject("Operation failed!");
+//         }
+//     }, 2000);
+// });
 
-const res = myfnc()
-
-console.log(res)
+// myPromise.then(
+//     result => {
+//         console.log("Success:", result);
+//     },
+//     error => {
+//         console.error("Error:", error);
+//     }
+// );
