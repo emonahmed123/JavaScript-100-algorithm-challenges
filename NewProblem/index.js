740d3a349e2d670e342bec7d930b8a14753ab915
Emon Ahmed <imonshomon@gmail.com>
2024-06-22 23:09:28 +0600

    done

--- NewProblem/index.js
+++ NewProblem/index.js
@@ -1,27 +1,52 @@
 // 1- Given a string ,reverse each word in the sentence
 
 
-const str = "hello world  it very hard"
+// const str = "hello world"
 
-const ReversWord = (sentence) => {
-    let worlds = sentence.split(' ')
+// const Last = str.split(' ');
+// let resss = []
+// for (let i = 0; i < Last.length; i++) {
+//     const reverseW = Last[i]
+//     const bal = reverseW.split('').reverse().join('')
+//     resss.push(bal)
+// }
+// console.log(resss.join(' '))
 
-    let result = [];
+// const ReversWord = (sentence) => {
+//     let worlds = sentence.split(' ')
 
-    for (let i = 0; i < worlds.length; i++) {
-        let resultWorld = ''
-        let world = worlds[i]
+//     let result = [];
 
-        for (let j = world.length - 1; j >= 0; j--) {
+//     for (let i = 0; i < worlds.length; i++) {
+//         let resultWorld = ''
+//         let world = worlds[i]
 
-            resultWorld += world[j]
-        }
-        result.push(resultWorld)
+//         for (let j = world.length - 1; j >= 0; j--) {
+
+//             resultWorld += world[j]
+//         }
+//         result.push(resultWorld)
+//     }
+
+//     console.log(result.join(' '))
+
+
+// }
+
+// const resultWorld = ReversWord(str)
+
+// 2- How to check if an object is an array or not Provide some code.
+
+
+const CheakArry = (params) => {
+
+    if (Array.isArray(params) === true) {
+        console.log('this is array')
     }
 
-    console.log(result.join(' '))
-
-
+    else {
+        console.log('no')
+    }
 }
 
-const resultWorld = ReversWord(str)
+CheakArry([])
