9dbde9cae3d2b1d8c74821dab7b39eb3c83640ab
Emon Ahmed <imonshomon@gmail.com>
2025-01-01 17:25:48 +0600

    done T-003 problem

--- arraytask.js
+++ arraytask.js
@@ -1,12 +1,26 @@
 /*   
-
 T-001: Create an array of 5 elements using the Array Constructor.
-
-
 */
 //Ans:001
 
-const arr = new Array('emon', 'sumon', 'alif', 'ss', "jkk")
+// const arr = new Array("emon", "sumon", "alif", "ss", "jkk");
 
-console.log(arr)
+/*
+Create an array of 3 empty slots.
+
+*/
+//Ans:002
+
+// console.log(new Array(3))
+
+/* 
+T-003: Create an array of 6 elements using the Array literals and access the fourth element in the array using its length property.
+*/
+const myArray = [10, 20, 30, 40, 50, 60];
+
+const fourthElement = myArray[myArray.length - (myArray.length - 3)]
+
+console.log(fourthElement)
+
+
 
