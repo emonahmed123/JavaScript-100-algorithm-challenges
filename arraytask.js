128129ffda305ba06f941fe74d57677aa8f4c146
Emon Ahmed <imonshomon@gmail.com>
2025-01-02 10:54:13 +0600

    solve 004 problem

--- arraytask.js
+++ arraytask.js
@@ -1,4 +1,4 @@
-/*   
+/*
 T-001: Create an array of 5 elements using the Array Constructor.
 */
 //Ans:001
@@ -13,14 +13,26 @@ Create an array of 3 empty slots.
 
 // console.log(new Array(3))
 
-/* 
+/*
 T-003: Create an array of 6 elements using the Array literals and access the fourth element in the array using its length property.
 */
-const myArray = [10, 20, 30, 40, 50, 60];
+// const myArray = [10, 20, 30, 40, 50, 60];
 
-const fourthElement = myArray[myArray.length - (myArray.length - 3)]
+// const fourthElement = myArray[myArray.length - (myArray.length - 3)]
 
-console.log(fourthElement)
+// console.log(fourthElement)
 
 
 
+/* 
+ T-004: Use the for loop on the above array to print elements in the odd index
+*/
+//ans:004
+
+const myArray = [10, 1, 30, 40, 50, 60];
+
+for (let i = 0; i < myArray.length; i++) {
+    if (i % 2 !== 0) {
+        console.log(myArray[i])
+    }
+}
