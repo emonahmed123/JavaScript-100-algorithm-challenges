e573f50599b4f5d5ecd8981a4111383419bd5222
Emon Ahmed <imonshomon@gmail.com>
2025-08-10 11:33:27 +0600

    solve inverted Half pyramid

---
+++ problem-7.js
@@ -0,0 +1,66 @@
+
+
+const row = 5;
+const col = 5;
+let start = ""
+for (let i = 1; i <= row; i++) {
+    for (let j = 1; j <= col; j++) {
+        start += "*"
+    }
+    start += "\n"
+
+}
+
+console.log(start);
+
+
+
+
+
+const row = 5;
+const col = 5;
+let start = ""
+for (let i = 1; i <= row; i++) {
+    for (let j = 1; j <= col; j++) {
+
+
+        if (j == 1 || j == col) {
+            start += " * "
+        }
+        else if (i == 1 || i == row) {
+            start += " * "
+        }
+        else {
+            start += "   "
+        }
+
+    }
+    start += "\n"
+
+}
+
+console.log(start);
+
+
+
+const row = 5;
+// const col = 5;
+
+let start = ""
+
+for (let i = row; i >= 1; i--) {
+    console.log(i)
+    for (let j = 1; j <= i; j++) {
+        console.log(j)
+        start += "*";
+
+
+
+    }
+    start += "\n"
+
+}
+
+
+console.log(start)
+
