0e42a5468c3580dd460cb14a5d787e3f831b8a31
Emon Ahmed <imonshomon@gmail.com>
2025-10-17 15:55:55 +0600

    how to add key and value in object

--- 40-Day-Javascript/day-12-object/index.js
+++ 40-Day-Javascript/day-12-object/index.js
@@ -1,1 +1,17 @@
 // Learn about objects in JavaScript
+
+console.log('data ');
+
+
+let user = {
+    name: 'emon',
+    age: 20,
+    "is Admin": true
+};
+user.["Movie Lover"] = true;
+
+user.isSeniorCitizen = false;
+console.log(user.name); // emon
+console.log(user.age);  // 20
+console.log(user); // false
+console.log(user["is Admin"]); // true
