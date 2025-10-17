07f8f40621865a6ad4c9764321518569b5901bea
Emon Ahmed <imonshomon@gmail.com>
2025-10-17 16:04:46 +0600

    how to  dynamic property

--- 40-Day-Javascript/day-12-object/index.js
+++ 40-Day-Javascript/day-12-object/index.js
@@ -8,10 +8,20 @@ let user = {
     age: 20,
     "is Admin": true
 };
-user.["Movie Lover"] = true;
+user["Movie Lover"] = true;
 
 user.isSeniorCitizen = false;
 console.log(user.name); // emon
 console.log(user.age);  // 20
 console.log(user); // false
 console.log(user["is Admin"]); // true
+
+// Deleting a property
+// delete user["Movie Lover"];//
+
+
+
+// dynamic property access
+
+const somekey = "age";
+console.log(user[somekey]); // 20
