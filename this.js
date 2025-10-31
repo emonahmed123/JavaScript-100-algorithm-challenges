974a984e33b8c5ef0ae394904ef294095ecbc790
Emon Ahmed <imonshomon@gmail.com>
2025-10-31 11:07:13 +0600

    done explicit binding

--- this.js
+++ this.js
@@ -9,20 +9,52 @@
 // }
 
 
+// var emon = {
+//     name: "emon",
+//     age: 35,
+
+// }
+
+
+
+// var printPlayerNameFun = function (obj) {
+//     obj.printPlayerName = function () {
+//         console.log(this.name)
+//     }
+// }
+
+// printPlayerNameFun(emon)
+
+// emon.printPlayerName()
+
+
+
+
+// explict binding
+
+
+// var sk
+
+var printName = function (v1) {
+    console.log(`${this.name} very`)
+}
+
 var emon = {
     name: "emon",
     age: 35,
 
+
 }
 
+var v1 = 'Handsome'
+var v2 = 'good'
 
+var v = [v1, v2]
 
-var printPlayerNameFun = function (obj) {
-    obj.printPlayerName = function () {
-        console.log(this.name)
-    }
-}
+// printName.call(emon, v1)
 
-printPlayerNameFun(emon)
+// printName.apply(emon, v1)
 
-emon.printPlayerName()
+// var newfunc = printName.bind(sakib, v1)
+
+// newfunc()
