7f084fd4f50387fd187a341d25bafe0a539e63d0
Emon Ahmed <imonshomon@gmail.com>
2025-10-31 14:32:48 +0600

    done this

--- this.js
+++ this.js
@@ -35,21 +35,21 @@
 
 // var sk
 
-var printName = function (v1) {
-    console.log(`${this.name} very`)
-}
+// var printName = function (v1) {
+//     console.log(`${this.name} very`)
+// }
 
-var emon = {
-    name: "emon",
-    age: 35,
+// var emon = {
+//     name: "emon",
+//     age: 35,
 
 
-}
+// }
 
-var v1 = 'Handsome'
-var v2 = 'good'
+// var v1 = 'Handsome'
+// var v2 = 'good'
 
-var v = [v1, v2]
+// var v = [v1, v2]
 
 // printName.call(emon, v1)
 
@@ -58,3 +58,68 @@ var v = [v1, v2]
 // var newfunc = printName.bind(sakib, v1)
 
 // newfunc()
+
+
+
+//new binding
+
+
+
+// var PrintName 
+
+// const obj = {
+//     name: "Tom",
+//     greet: function () {
+//         console.log(`Hello, ${this.name}!`);
+//     },
+// };
+// const greetFn = obj.greet;
+
+
+// // console.log(greetFn.call(obj))
+// greetFn.call(obj)
+
+
+
+// function outer(a) {
+//     console.log("this inside an outer function", this);
+
+//     return function inner(b) {
+//         console.log("this inside an inner function", this);
+//     }
+// }
+// const outerResult = outer(5);
+// outerResult(3)
+
+
+
+// const obj = {
+//     name: "emon ",
+//     age: 33,
+//     payName: function () {
+
+//         return () => console.log(this.name)
+//     }
+// }
+
+
+
+// const result = obj.payName()
+
+// result()
+
+// // console.log(result.apply(obj))
+
+
+const user = {
+    name: "Alex",
+    greet: function () {
+        // name = 'emon'
+        function inner() {
+            console.log(`Hello, ${this.name}!`);
+        }
+        inner.call(user);
+    },
+};
+
+const result = user.greet()
