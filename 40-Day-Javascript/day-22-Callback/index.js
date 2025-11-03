832afa82f2315e91775b3826440890a46776302b
Emon Ahmed <imonshomon@gmail.com>
2025-11-03 11:28:57 +0600

    how to work callBack

--- 40-Day-Javascript/day-22-Callback/index.js
+++ 40-Day-Javascript/day-22-Callback/index.js
@@ -1,18 +1,40 @@
-console.log("Day 22: Callback Functions");
+// console.log("Day 22: Callback Functions");
+
+// // for (let i = 1; i <= 5; i++) {
+// //     console.log('count')
+
+// // }
+// // callback function
+// function fetchData(callback) {
+//     console.log("Fetching data...");
+//     setTimeout(() => {
+//         const data = { id: 1, name: "Sample Data" };
+//         callback(data);
+//     }, 2000);
+// }
 
 
-function fetchData(callback) {
-    console.log("Fetching data...");
-    setTimeout(() => {
-        const data = { id: 1, name: "Sample Data" };
-        callback(data);
-    }, 2000);
+// fetchData(function (data) {
+//     console.log("Data received:", data);
+// });
+
+
+// console.log("End of Day 22");
+
+
+
+
+function greet(name, callback) {
+
+    console.log("hello")
+
+
+    setTimeout(() => { callback(); }, 2000)
+    console.log("greeted")
 }
 
+function sayGoodbye() {
+    console.log("Goodbye!");
+}
 
-fetchData(function (data) {
-    console.log("Data received:", data);
-});
-
-
-console.log("End of Day 22");
+greet("Alice", sayGoodbye);
